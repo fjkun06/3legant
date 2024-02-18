f333a09a0e800f91ef792d9a844af1ac4d9a88d2
= <=>
2024-02-18 21:17:50 +0100

    Basics

--- 3legant/src/app/page.tsx
+++ 3legant/src/app/page.tsx
@@ -1,11 +1,10 @@
+import { SearchIcon } from "@/components/icons";
 
 export default function Home() {
   return (
     <main>
-        <p>
-          3legant
-        </p>
-
+      <p>3legant</p>
+      <SearchIcon size={24} />
     </main>
   );
 }
--- 3legant/src/components/icons/SearchIcon.tsx
+++ 3legant/src/components/icons/SearchIcon.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const SearchIcon = () => {
+import { Icon } from "./interface";
+
+/**
+ * Search Icon
+ *
+ * @param {number} [size]  - icon size (width and height).
+ *
+ * @param {string} [color] - color ist always set to currentColor.
+ *
+ *@example
+ * //Using Icon with just size and default color value.
+ *  <SearchIcon size={24} />
+ * */
+
+const SearchIcon = ({ size = 24, color = "currentColor" }: Icon) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-<path d="M18.5 18.5L22 22M21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21C16.7467 21 21 16.7467 21 11.5Z" stroke="#141718" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-  )
-}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M18.5 18.5L22 22M21 11.5C21 6.25329 16.7467 2 11.5
+         2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5
+          21C16.7467 21 21 16.7467 21 11.5Z"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
-export default SearchIcon
+export default SearchIcon;
